680918b3d8fb17abf9cc33ed61026eed7bed96a2
kaiowolf <kaioexpert@hotmail.com>
2019-10-19 17:46:58 -0300

    Alteracao Formulario
    Criação de Formulario

--- site-cursos/src/pages/contato/index.js
+++ site-cursos/src/pages/contato/index.js
@@ -1,10 +1,13 @@
 import React, { Component } from 'react';
+import Cabecalho from '../../componentes/menu/cabecalho'
+
 export default class Contato extends Component {
-render() {
-return (
-<div className="container">
-<h1>Classe Contato</h1>
-</div>
-)
-}
+    render() {
+    return (
+        <div className="container">
+            <Cabecalho titulo="Contato"
+                subtitulo="entre em Contato conosco" />
+        </div>
+            )
+    }
 }
--- site-cursos/src/pages/cursos/index.js
+++ site-cursos/src/pages/cursos/index.js
@@ -1,10 +1,15 @@
 import React, { Component } from 'react';
+import Cabecalho from '../../componentes/menu/cabecalho'
+import Cadastro from './cadastro'
+
 export default class Curso extends Component {
-render() {
-return (
-<div className="container">
-<h1>Classe Curso</h1>
-</div>
-)
-}
+    render() {
+      return (
+        <div className="container">
+            <Cabecalho titulo="Cursos"
+                subtitulo="cadastro de cursos" />
+                <Cadastro></Cadastro>
+        </div>
+    )
+  }
 }
